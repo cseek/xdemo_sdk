6832ca1bebc9b9cc9d78c1b58dec3f9fdba2ab52
aurson <jassimxiong@gmail.com>
2025-06-08 17:49:48 +0800

    feat: 增加make_unique

--- cmake/ToolchainX64.cmake
+++ cmake/ToolchainX64.cmake
@@ -2,6 +2,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86)
 # 设置 C/C++ 编译器
-set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD 11)
 set(CMAKE_C_COMPILER /usr/bin/gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/g++)
